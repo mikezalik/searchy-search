f902a430d138ca3f7c9bb24f025cccf1e1eb95b8
mikezalik <mike.zalik@gmail.com>
2021-01-16 11:04:50 -0800

    Adds Devise user model, views, migrations and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'search_items#index'
   resources :search_items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
